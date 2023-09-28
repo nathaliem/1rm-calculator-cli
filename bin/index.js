4cb34f2d91345832c413b4a25fe342561a66903a
Nathalie Maes <nathaliemaes92@gmail.com>
2023-09-28 11:28:12 +0200

    Initial push

---
+++ bin/index.js
@@ -0,0 +1,55 @@
+#!/usr/bin/env node
+const yargsInteractive = require('yargs-interactive');
+const chalk = require('chalk');
+const boxen = require('boxen');
+const options = {
+  interactive: { default: true },
+  weight: { type: 'input', describe: 'Enter the weight of the lift (in kg)' },
+  reps: { type: 'input', describe: 'Enter the amount of reps' },
+};
+const theme = {
+    green: '#50C878',
+    red: '#E0115F',
+};
+
+console.log(boxen('ONE REP MAX CALCULATOR', { borderColor: theme.red, borderStyle: 'classic', padding: 2, margin: 1, color: 'white' }));
+
+console.log(chalk.hex(theme.green)(
+    `⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⣶⣿⣿⣿⣿⣷⣦⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢾⠿⡏⢿⡟⠿⠁⢸⣿⣷⡄⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣁⣀⡀⢠⣶⠶⠀⠁⣽⣿⣆⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠁⠀⠀⠀⠀⠀⠘⣿⣿⣷⡀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿⣿⣆⠀⠀⠀⠀
+    ⠀⠀⢀⣠⣴⣶⣶⣶⣦⣄⠀⠀⠀⠀⢀⣀⣀⣀⠀⠀⠀⣼⣿⣿⣿⣿⣧⠀⠀⠀
+    ⠀⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣐⢾⣿⣿⣿⣿⣷⣦⣌⡻⣿⣿⣿⣿⣿⣧⠀⠀
+    ⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⣿⣿⣿⣿⡇⠀
+    ⠀⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⢋⡉⠛⠿⣿⣿⣿⣿⣿⡿⢿⣿⣿⣿⣿⣿⣿⡇⠀
+    ⠀⣿⣿⣿⣿⣿⣿⣿⣿⡏⢠⣿⣿⣷⡦⠀⣈⣉⣀⣤⣶⣿⣟⣛⠛⠛⠛⠛⠃⠀
+    ⠀⣿⣿⣿⣿⣿⣿⣿⡿⠀⠾⠛⠋⠁⠐⠺⠿⠿⠿⠛⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀
+    ⠀⣿⣿⣿⣿⣿⣿⠟⢁⣴⣶⣾⠇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⣿⣿⣿⣿⠿⠋⣰⣿⣿⣿⠏⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠛⠛⠋⠁⠐⠛⠛⠛⠛⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀`));
+
+console.log(chalk.hex(theme.green)(`
+-------------------------------------------------------------------------------------------
+`));
+console.log(`Calculate the ${chalk.hex(theme.red).bold('max weight you can lift')} for a single repetition of an exercise.`);
+console.log(chalk.hex(theme.green)(`
+-------------------------------------------------------------------------------------------
+`));
+
+yargsInteractive()
+  .usage('$0 <command>')
+  .interactive(options)
+  .then(result => {
+    let { weight, reps } = result;
+
+    weight = weight.replace(/,/g, '.').replace(/[^0-9\.]+/g, '');
+    reps = reps.replace(/,/g, '.');
+
+    const max = (weight / (1.0278 - 0.0278 * reps)).toFixed(2);
+    const styledMax = chalk.hex(theme.green).bold(`${max} kg`);
+
+    console.log(boxen(`Your one rep max is estimated to be ${styledMax}`, { padding: 1, borderStyle: 'double', borderColor: theme.red }));
+  });
---
+++ readme.md
@@ -0,0 +1,5 @@
+# One rep max calculator
+> Calculate the max weight you can lift for a single repetition of an exercise, inside your terminal.
+
+## How to use
+`$ 1rm`
